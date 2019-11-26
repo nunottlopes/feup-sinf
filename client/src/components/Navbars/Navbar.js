864a0b5de23f18b0fa095c482ad93745defd7b27
duartefrazao <duartemarquesmano@gmail.com>
2019-11-26 17:37:54 +0000

    Changes made in class to remove unnused components

--- client/src/components/Navbars/Navbar.js
+++ client/src/components/Navbars/Navbar.js
@@ -46,15 +46,7 @@ export default function Header(props) {
         <Hidden smDown implementation="css">
           {props.rtlActive ? <RTLNavbarLinks /> : <AdminNavbarLinks />}
         </Hidden>
-        <Hidden mdUp implementation="css">
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={props.handleDrawerToggle}
-          >
-            <Menu />
-          </IconButton>
-        </Hidden>
+        
       </Toolbar>
     </AppBar>
   );
