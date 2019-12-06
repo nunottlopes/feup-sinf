300f4e44b954264e57c0297d3bc496c0788d4b93
Carolina Soares <up201605010@fe.up.pt>
2019-12-06 23:05:19 +0000

    finished side bar

--- client/src/layouts/Admin.js
+++ client/src/layouts/Admin.js
@@ -90,7 +90,7 @@ export default function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={"Company Dashboard"}
+        logoText={"360º CompanyDashboard"}
         className={classes.sideBar}
         //logo={logo}
         handleDrawerToggle={handleDrawerToggle}
@@ -98,26 +98,13 @@ export default function Admin({ ...rest }) {
         color={color}
         {...rest}
       />
+      // classes.mainPanel é um problema, try
       <div className={classes.mainPanel} ref={mainPanel}>
         <Navbar
+          color="danger"
           routes={routes}
-          handleDrawerToggle={handleDrawerToggle}
+          //handleDrawerToggle={handleDrawerToggle}
           {...rest}
-        />
-        {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
-        {getRoute() ? (
-          <div className={classes.content}>
-            <div className={classes.container}>{switchRoutes}</div>
-          </div>
-        ) : (
-          <div className={classes.map}>{switchRoutes}</div>
-        )}
-        {getRoute() ? <Footer /> : null}
-        <FixedPlugin
-          handleColorClick={handleColorClick}
-          bgColor={color}
-          handleFixedClick={handleFixedClick}
-          fixedClasses={fixedClasses}
         />
       </div>
     </div>
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,25 +1,14 @@
-/*!
-
-=========================================================
-* Material Dashboard React - v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 // @material-ui/icons
-import { Home, TrendingUp, EuroSymbol, Store, ShoppingCart } from "@material-ui/icons";
+import {
+  Home,
+  TrendingUp,
+  EuroSymbol,
+  Store,
+  ShoppingCart
+} from "@material-ui/icons";
 
 // core components/views for Admin layout
-import { Overview, Financial, Sales, Inventory, Purchases } from './views/';
+import { Overview, Financial, Sales, Inventory, Purchases } from "./views/";
 
 const dashboardRoutes = [
   {
@@ -56,7 +45,7 @@ const dashboardRoutes = [
     icon: ShoppingCart,
     component: Purchases,
     layout: "/admin"
-  },
+  }
 ];
 
 export default dashboardRoutes;
