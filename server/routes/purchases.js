bdebde07c42d112e9f217096aae9c272eb23591f
duartefrazao <duartemarquesmano@gmail.com>
2019-12-16 21:57:15 +0000

    Revert changes of rebase for purchases

--- server/routes/purchases.js
+++ server/routes/purchases.js
@@ -3,110 +3,34 @@ var router = express.Router();
 const getJasminAPI = require("../config/jasmin").getJasminAPI;
 const { readDocuments } = require("../mongodb/actions");
 
-/* PURCHASES */
-router.get(`/suppliers`, function(req, res) {
-  getJasminAPI("/purchasesCore/purchasesItems/extension")
-    .then(resp => {
-      res.send(JSON.parse(resp));
-    })
-    .catch(err => {
-      res.send(err);
-    });
-});
-
-//Pode ser paged
-router.get(`/`, function(req, res) {
-  getJasminAPI("/invoiceReceipt/expenses")
-    .then(resp => {
-      res.send(resp);
-    })
-    .catch(err => {
-      res.send(err);
-    });
-});
-
-router.get(`/pendent-bills/:account`, function(req, res) {
-  readDocuments("GeneralLedgerEntries", "", response => {
-    let journals = response[0];
-    let credit = {};
-    let debit = {};
-    let startDate =
-      "start-date" in req.query ? new Date(req.query["start-date"]) : null;
-    let endDate =
-      "end-date" in req.query ? new Date(req.query["end-date"]) : null;
-    let account = req.params.account;
-    let topSuppliers = {};
-    if (journals.Journal == undefined) return { credit: 0, debit: 0 };
-    journals.Journal.forEach(journal => {
-      if (journal.Transaction != undefined) {
-        if (Array.isArray(journal.Transaction)) {
-          journal.Transaction.forEach(transaction => {
-            let result = processTransaction(
-              transaction,
-              account,
-              startDate,
-              endDate
-            );
-            if (result.companies != undefined) {
-              for (const [company, value] of Object.entries(result.companies)) {
-                if (value.debit > 0 || value.credit > 0) {
-                  if (topSuppliers[company] == undefined) {
-                    topSuppliers[company] = {
-                      debit: value.debit,
-                      credit: value.credit
-                    };
-                  } else {
-                    topSuppliers[company].debit += value.debit;
-                    topSuppliers[company].credit += value.credit;
-                  }
-                }
-              }
-            }
-          });
-        } else {
-          let result = processTransaction(
-            journal.Transaction,
-            account,
-            startDate,
-            endDate
-          );
-          if (result.companies != undefined) {
-            for (const [company, value] of Object.entries(result.companies)) {
-              if (value > 0) {
-                if (topSuppliers[company] == undefined) {
-                  topSuppliers[company] = {
-                    debit: value.debit,
-                    credit: value.credit
-                  };
-                } else {
-                  topSuppliers[company].debit += value.debit;
-                  topSuppliers[company].credit += value.credit;
-                }
-              }
-            }
-          }
-        }
-      }
-    });
-    res.send({ topSuppliers, credit, debit, response });
-  });
-});
-
-router.get(`/orders-delivered`, function(req, res) {
-  getJasminAPI("/shipping/deliveries")
+// All orders
+router.get(`/all-orders`, function(req, res) {
+  getJasminAPI("/purchases/orders")
     .then(response => {
       let orders = [];
       response = JSON.parse(response);
       for (let i = 0; i < response.length; i++) {
         if (!response[i].isDeleted) {
+          let items = [];
+
+          response[i].documentLines.forEach(element => {
+            items.push({
+              quantity: element.quantity,
+              totalAmount: element.lineExtensionAmount.amount,
+              warehouse: element.warehouse,
+              itemId: element.purchasesItem,
+              itemDescription: element.purchasesItemDescription,
+              deliveryDate: element.deliveryDate
+            });
+          });
+
           orders.push({
             name: response[i].accountingPartyDescription,
             date: response[i].exchangeRateDate,
-            id: response[i].naturalKey,
-            entity: response[i].accountingParty,
+            orderId: response[i].naturalKey,
+            supplierId: response[i].accountingParty,
             payableAmout: response[i].payableAmount.amount,
-            salesItem: response[i].documentLines[0].item
-            // deleted: response[i].isDeleted
+            items: items
           });
         }
       }
@@ -117,22 +41,22 @@ router.get(`/orders-delivered`, function(req, res) {
     });
 });
 
-// TODO: REMOVE ORDERS ALREADY DELIVERED
-router.get(`/orders-to-deliver`, function(req, res) {
-  getJasminAPI("/shipping/shippingRequests")
+// TABLE_03 Waiting orders
+router.get(`/orders-to-receive`, function(req, res) {
+  getJasminAPI("/goodsReceipt/processOrders/1/10?company=")
     .then(response => {
       let orders = [];
       response = JSON.parse(response);
+      // res.send(response);
       for (let i = 0; i < response.length; i++) {
         if (!response[i].isDeleted) {
-          orders.push(response[i]);
-          // orders.push({
-          //   name: response[i].accountingPartyDescription,
-          //   date: response[i].exchangeRateDate,
-          //   id: response[i].naturalKey,
-          //   entity: response[i].accountingParty,
-          //   payableAmout: response[i].payableAmount.amount
-          // });
+          orders.push({
+            item: response[i].item,
+            itemDescription: response[i].itemDescription,
+            quantity: response[i].quantity,
+            deliveryDate: response[i].deliveryDate,
+            sourceDocKey: response[i].sourceDocKey
+          });
         }
       }
       res.send(orders);
@@ -141,129 +65,123 @@ router.get(`/orders-to-deliver`, function(req, res) {
       res.send(err);
     });
 });
-/* 
-processTransaction = (transaction, account, startDate, endDate) => {
-  function processLine(line, type) {
-    if ((line.AccountID + " ").indexOf(account) != 0) return 0;
-    return type == "credit" ? line.CreditAmount : line.DebitAmount;
-  }
 
-  let date = new Date(transaction.TransactionDate);
-  if (
-    (startDate != null && date < startDate) ||
-    (endDate != undefined && date > endDate)
-  ) {
-    return { credit: 0, debit: 0 };
-  }
+// LINEAR_01 Expenses
+router.get(`/expenses`, function(req, res) {
+  let startDate =
+    "start-date" in req.query ? new Date(req.query["start-date"]) : null;
+  let endDate =
+    "end-date" in req.query ? new Date(req.query["end-date"]) : null;
 
-  let credit = 0;
-  let debit = 0;
+  let expenses = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  getJasminAPI("/invoiceReceipt/expenses")
+    .then(response => {
+      response = JSON.parse(response);
 
-  let lines = transaction.Lines;
-
-  let companies = {};
-
-  if (lines.CreditLine) {
-    if (Array.isArray(lines.CreditLine)) {
-      lines.CreditLine.forEach(line => {
-        let companyCredit = processLine(line, "credit");
-
-        credit += companyCredit;
-        if (companies[line.AccountID] == undefined) {
-          companies[line.AccountID] = { credit: companyCredit, debit: 0 };
-        } else {
-          companies[line.AccountID].credit += companyCredit;
+      for (let i = 0; i < response.length; i++) {
+        let dueDate = new Date(response[i].dueDate);
+        if (
+          !response[i].isDeleted &&
+          (startDate == null || startDate <= dueDate) &&
+          (endDate == null || dueDate <= endDate)
+        ) {
+          expenses[dueDate.getMonth()] += response[i].payableAmount.amount;
         }
-      });
-    } else {
-      let companyCredit = processLine(lines.CreditLine, "credit");
-
-      credit += companyCredit;
-
-      if (companies[lines.CreditLine.AccountID] == undefined) {
-        companies[lines.CreditLine.AccountID] = {
-          credit: companyCredit,
-          debit: 0
-        };
-      } else {
-        companies[lines.CreditLine.AccountID].credit += companyCredit;
       }
-    }
-  }
+      res.send({ data: expenses, label: "Expenses per Month" });
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
-  if (lines.DebitLine) {
-    if (Array.isArray(lines.DebitLine)) {
-      lines.DebitLine.forEach(line => {
-        let companyDebit = processLine(line, "debit");
+// TABLE_02
+router.get(`/pendent-bills`, function(req, res) {
+  getJasminAPI("/invoiceReceipt/invoices")
+    .then(response => {
+      let bills = [];
+      response = JSON.parse(response);
+      for (let i = 0; i < response.length; i++) {
+        if (!response[i].isDeleted && response[i].documentStatus !== 2) {
+          let items = [];
 
-        debit += companyDebit;
+          response[i].documentLines.forEach(element => {
+            items.push({
+              quantity: element.quantity,
+              totalAmount: element.lineExtensionAmount.amount,
+              warehouse: element.warehouse,
+              itemId: element.purchasesItem,
+              itemDescription: element.purchasesItemDescription,
+              deliveryDate: element.deliveryDate
+            });
+          });
 
-        if (companies[line.AccountID] == undefined) {
-          companies[line.AccountID] = { debit: companyDebit, credit: 0 };
-        } else {
-          companies[line.AccountID].debit += companyDebit;
+          bills.push({
+            dueDate: response[i].dueDate,
+            amount: response[i].payableAmount.amount,
+            entity: response[i].accountingParty,
+            supplier: response[i].accountingPartyDescription,
+            orderId: response[i].naturalKey,
+            items: items
+          });
         }
-      });
-    } else {
-      let companyDebit = processLine(lines.DebitLine, "debit");
-
-      debit += companyDebit;
-
-      if (companies[lines.DebitLine.AccountID] == undefined) {
-        companies[lines.DebitLine.AccountID] = {
-          debit: companyDebit,
-          credit: 0
-        };
-      } else {
-        companies[lines.DebitLine.AccountID].debit = companyDebit;
       }
-    }
-  }
+      res.send(bills);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
-  return { credit, debit, companies };
-};
- */
-/* processTransaction = (transaction,account,startDate,endDate) => {
+// TABLE_01
+router.get(`/suppliers`, function(req, res) {
+  let startDate =
+    "start-date" in req.query ? new Date(req.query["start-date"]) : null;
+  let endDate =
+    "end-date" in req.query ? new Date(req.query["end-date"]) : null;
 
-  function processLine(line,type){
-    //Não é fornecedores
-    if((line.AccountID + " ").indexOf(account) != 0)
-      return 0
-    return type == "credit" ? line.CreditAmount : line.DebitAmount;
-  }
+  getJasminAPI("/invoiceReceipt/expenses")
+    .then(response => {
+      response = JSON.parse(response);
 
-  let date = new Date(transaction.TransactionDate);
+      let suppliers = {};
 
-  if((startDate != null && date < startDate) || (endDate != undefined && date > endDate)){
-    return {credit:0,debit:0}
-  }
+      for (let i = 0; i < response.length; i++) {
+        let dueDate = new Date(response[i].dueDate);
+        if (
+          !response[i].isDeleted &&
+          (startDate == null || startDate <= dueDate) &&
+          (endDate == null || dueDate <= endDate)
+        ) {
+          try {
+            if (suppliers.hasOwnProperty(response[i].sellerSupplierParty)) {
+              suppliers[response[i].sellerSupplierParty].totalExpenses +=
+                response[i].payableAmount.amount;
+            } else {
+              suppliers[response[i].sellerSupplierParty] = {
+                name: response[i].sellerSupplierPartyDescription,
+                totalExpenses: response[i].payableAmount.amount
+              };
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      }
 
-  let credit = 0;
-  let debit = 0;
+      suppliers = Object.keys(suppliers)
+        .sort((a, b) => suppliers[b].totalExpenses - suppliers[a].totalExpenses)
+        .map(elem => ({
+          id: elem,
+          name: suppliers[elem].name,
+          totalExpenses: suppliers[elem].totalExpenses
+        }));
 
-  let lines = transaction.Lines;
-
-
-  if(lines.CreditLine){
-    if(Array.isArray(lines.CreditLine)){
-      credit+= lines.CreditLine.map(line => {
-        return processLine(line,"credit");
-      }).reduce((n1,n2) => n1+n2);
-    }else{
-      credit+=processLine(lines.CreditLine,"credit");
-    }
-  }
-
-  if(lines.DebitLine){
-    if(Array.isArray(lines.DebitLine)){
-      debit+= lines.DebitLine.map(line => {
-        return processLine(line,"debit");
-      }).reduce((n1,n2) => n1+n2);
-    }else{
-      debit+=processLine(lines.DebitLine,"debit");
-    }
-  }
-  return {credit,debit }
-} */
+      res.send(suppliers);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 
 module.exports = router;
