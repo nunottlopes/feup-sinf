385fac73e0191d2b987744577f9992f2e4e33ec9
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-12-17 13:32:37 +0000

    feat: Implement Rechart graphs in Overview page

--- client/src/views/Overview/FinancesGraph.js
+++ client/src/views/Overview/FinancesGraph.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ChartistGraph from 'react-chartist';
+import { LineChart, Line, Legend, CartesianGrid, XAxis, YAxis, ResponsiveContainer, Tooltip } from 'recharts';
 
 const FinancesGraph = (props) => {
   const { expenses, income } = props;
-
-  const data = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    series: [
-      {
-        className: 'series-expenses',
-        data: expenses
-      },
-      {
-        className: 'series-income',
-        data: income
-      },
-    ]
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  let data = [];
+  for (let i = 0; i < months.length; i++) {
+    data.push({
+      month: months[i],
+      expense: expenses[i],
+      income: income[i]
+    })
   }
 
-  const options = {
-    height: 400
-  }
-
-  return <ChartistGraph type='Line' data={data} options={options}></ChartistGraph>
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="expense" stroke="red" />
+        <Line type="monotone" dataKey="income" stroke="green" />
+      </LineChart>
+    </ResponsiveContainer>
+  )
 }
 
 FinancesGraph.propTypes = {
