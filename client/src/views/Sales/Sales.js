9709465342b5b842215c0c483f7ee6397d65412c
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-12-17 15:04:57 +0000

    feat: Implement Rechart in Financial page

--- client/src/views/Sales/Sales.js
+++ client/src/views/Sales/Sales.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 import Skeleton from "@material-ui/lab/Skeleton";
-import ChartistGraph from "react-chartist";
-import { Typography } from "@material-ui/core";
-import { MyPieChart } from "./../../components";
-import TopProductsTable from "./TopProductsTable";
-import TopClientsTable from "./TopClientsTable";
-
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { formatCurrency, formatNumber } from "../../utils";
+import { MyPieChart } from "./../../components";
+import TopClientsTable from "./TopClientsTable";
+import TopProductsTable from "./TopProductsTable";
+
+
 
 const axios = require("axios");
 
@@ -52,33 +53,40 @@ const __group_sales_by_month = daily_sales => {
  */
 const SalesVolumes = props => {
   const { net_sales, gross_sales } = props;
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
 
-  const data = {
-    labels: [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec"
-    ],
-    series: [net_sales, gross_sales]
-  };
+  const data = months.map((month, index) => ({
+    month: month,
+    net_sales: net_sales[index],
+    gross_sales: gross_sales[index]
+  }));
 
-  const options = {
-    height: 400,
-    axisY: {
-      labelInterpolationFnc: label => formatNumber(label, 3)
-    }
-  };
-
-  return <ChartistGraph type="Line" data={data} options={options} />;
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <BarChart data={data} margin={{ top: 5, right: 20, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis tickFormatter={(value) => formatNumber(value)} />
+        <Tooltip formatter={(value) => formatCurrency(value)} />
+        <Legend />
+        <Bar type="monotone" dataKey="net_sales" fill="red" />
+        <Bar type="monotone" dataKey="gross_sales" fill="green" />
+      </BarChart>
+    </ResponsiveContainer>
+  )
 };
 
 SalesVolumes.propTypes = {
@@ -92,33 +100,38 @@ SalesVolumes.propTypes = {
  */
 const CumulativeSalesVolumes = props => {
   const { cumulative_gross_sales } = props;
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
 
-  const data = {
-    labels: [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec"
-    ],
-    series: [cumulative_gross_sales]
-  };
+  const data = months.map((month, index) => ({
+    month: month,
+    cumulative_gross_sale: cumulative_gross_sales[index]
+  }));
 
-  const options = {
-    height: 400,
-    axisY: {
-      labelInterpolationFnc: label => formatNumber(label, 3)
-    }
-  };
-
-  return <ChartistGraph type="Line" data={data} options={options} />;
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <LineChart data={data} margin={{ top: 5, right: 20, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis tickFormatter={(value) => formatNumber(value)} />
+        <Tooltip formatter={(value) => formatCurrency(value)} />
+        <Legend />
+        <Line type="monotone" dataKey="cumulative_gross_sale" stroke="red" strokeWidth={2} />
+      </LineChart>
+    </ResponsiveContainer>
+  )
 };
 
 CumulativeSalesVolumes.propTypes = {
@@ -142,8 +155,8 @@ const InformationCard = props => {
           {formatCurrency(value)}
         </Typography>
       ) : (
-        <Skeleton variant="text" />
-      )}
+          <Skeleton variant="text" />
+        )}
     </Paper>
   );
 };
@@ -186,82 +199,82 @@ const Sales = () => {
     // Get the top regions
     axios
       .get(`${api_endpoint_base}/top-regions`)
-      .then(function(response) {
+      .then(function (response) {
         set_top_regions(response.data);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
     // Get the top products
     axios
       .get(`${api_endpoint_base}/top-products`)
-      .then(function(response) {
+      .then(function (response) {
         set_top_products(response.data);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
     // Get the top clients
     axios
       .get(`${api_endpoint_base}/top-clients`)
-      .then(function(response) {
+      .then(function (response) {
         set_top_clients(response.data);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
     // Get the daily volume sales for every month
     axios
       .get(`${api_endpoint_base}/daily-volume`)
-      .then(function(response) {
+      .then(function (response) {
         set_net_sales_volumes(__group_sales_by_month(response.data));
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
     // Get gross sales for every month and cumulative gross sales
     axios
       .get(`${api_endpoint_base}/cumulative-month-gross`)
-      .then(function(response) {
+      .then(function (response) {
         const [cumulative, per_month] = response.data;
         set_gross_cumulative_sales(cumulative.data);
         set_gross_sales_volumes(per_month.data);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
     // Get profit, revenues and cost of goods sold
     axios
       .get(`${api_endpoint_base}/profit`)
-      .then(function(response) {
+      .then(function (response) {
         set_profits(response.data);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
     // Get the total net sales
     axios
       .get(`${api_endpoint_base}/total-net-sales`)
-      .then(function(response) {
+      .then(function (response) {
         set_total_net_sales(response.data.totalNetSales);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
     // Get the total gross (net + taxes) sales
     axios
       .get(`${api_endpoint_base}/total-gross-sales`)
-      .then(function(response) {
+      .then(function (response) {
         set_total_gross_sales(response.data.totalGrossSales);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   }, []);
@@ -314,11 +327,11 @@ const Sales = () => {
           <Typography variant="h5" className={classes.graphs_title}>
             Sales Per Region
           </Typography>
-          <MyPieChart 
-            data={top_regions.slice(0, 5)} 
+          <MyPieChart
+            data={top_regions.slice(0, 5)}
             colors={['#bf211e', '#e82f2c', '#f95f5c', '#f99593', '#a06968']}
-            pieProps={{nameKey: 'id', dataKey: 'netTotal'}}
-            cellProps={{stroke: '#7f1614'}}
+            pieProps={{ nameKey: 'id', dataKey: 'netTotal' }}
+            cellProps={{ stroke: '#7f1614' }}
           />
         </Paper>
       </Grid>
