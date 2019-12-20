c29dca5385f77650077a55f9d0f9a1609eb84489
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-12-20 23:21:08 +0000

    feat: Override default MaterialUI theme colors

--- client/src/index.js
+++ client/src/index.js
@@ -19,21 +19,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-
+import { createMuiTheme } from '@material-ui/core/styles';
 // core components
 import Admin from "layouts/Admin.js";
 import { Login } from "views";
 import "assets/css/material-dashboard-react.css?v=1.8.0";
+import { ThemeProvider } from "@material-ui/styles";
 
 const hist = createBrowserHistory();
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#00ACC1' },
+  },
+});
+
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/admin" component={Admin} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </Router>,
+  <ThemeProvider theme={theme}>
+    <Router history={hist}>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/admin" component={Admin} />
+        <Redirect from="/" to="/admin/dashboard" />
+      </Switch>
+    </Router>
+  </ThemeProvider>,
   document.getElementById("root")
 );
