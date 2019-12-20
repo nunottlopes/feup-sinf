c5a3bf900cf04cfb5db404861895dbd11b816c54
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-12-20 14:06:36 +0000

    feat: Display progress spinners while data is being loaded

--- client/src/views/Overview/Overview.js
+++ client/src/views/Overview/Overview.js
@@ -1,4 +1,5 @@
 import { Typography } from "@material-ui/core";
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
@@ -30,16 +31,15 @@ const Overview = props => {
   // constant for the overview API endpoint
   const api_endpoint_base = "http://localhost:3001/api/overview";
   // hooks for data/state
-  const [top_clients, set_top_clients] = useState([]);
-  const [top_regions, set_top_regions] = useState([]);
-  const [top_products, set_top_products] = useState([]);
-  const [monthly_sales, set_monthly_sales] = useState([]);
+  const [top_clients, set_top_clients] = useState({ loaded: false, data: [] });
+  const [top_regions, set_top_regions] = useState({ loaded: false, data: [] });
+  const [top_products, set_top_products] = useState({ loaded: false, data: [] });
+  const [monthly_sales, set_monthly_sales] = useState({ loaded: false, data: [] });
   const [finances, set_finances] = useState({
-    revenue: {
-      data: []
-    },
-    cost: {
-      data: []
+    loaded: false,
+    data: {
+      revenue: [],
+      cost: []
     }
   });
   // Perform all API calls for this page
@@ -50,10 +50,10 @@ const Overview = props => {
         `${api_endpoint_base}/top-clients?start-date=${props.startDate}&end-date=${props.endDate}`,
         { withCredentials: true }
       )
-      .then(function(response) {
-        set_top_clients(response.data);
+      .then(function (response) {
+        set_top_clients({ loaded: true, data: response.data.slice(0, 5) });
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
@@ -63,10 +63,10 @@ const Overview = props => {
         `${api_endpoint_base}/top-regions?start-date=${props.startDate}&end-date=${props.endDate}`,
         { withCredentials: true }
       )
-      .then(function(response) {
-        set_top_regions(response.data);
+      .then(function (response) {
+        set_top_regions({ loaded: true, data: response.data.slice(0, 5) });
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
@@ -76,10 +76,10 @@ const Overview = props => {
         `${api_endpoint_base}/top-products?start-date=${props.startDate}&end-date=${props.endDate}`,
         { withCredentials: true }
       )
-      .then(function(response) {
-        set_top_products(response.data);
+      .then(function (response) {
+        set_top_products({ loaded: true, data: response.data.slice(0, 5) });
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
@@ -89,10 +89,10 @@ const Overview = props => {
         `${api_endpoint_base}/month-sales?start-date=${props.startDate}&end-date=${props.endDate}`,
         { withCredentials: true }
       )
-      .then(function(response) {
-        set_monthly_sales(response.data.data);
+      .then(function (response) {
+        set_monthly_sales({ loaded: true, data: response.data.data });
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
 
@@ -102,10 +102,11 @@ const Overview = props => {
         `${api_endpoint_base}/global-finances?start-date=${props.startDate}&end-date=${props.endDate}`,
         { withCredentials: true }
       )
-      .then(function(response) {
-        set_finances(response.data);
+      .then(function (response) {
+        console.log(response)
+        set_finances({ loaded: true, data: { revenue: response.data.revenue.data, cost: response.data.cost.data } });
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   }, [props.startDate, props.endDate]);
@@ -117,10 +118,14 @@ const Overview = props => {
           <Typography variant="h5" className={classes.graphs_title}>
             Global finances
           </Typography>
-          <FinancesGraph
-            income={finances.revenue.data}
-            expenses={finances.cost.data}
-          />
+          {finances.loaded ?
+            <FinancesGraph
+              income={finances.data.revenue}
+              expenses={finances.data.cost}
+            /> :
+            <CircularProgress />
+          }
+
         </Paper>
       </Grid>
       <Grid item lg={6} xs={12}>
@@ -128,7 +133,11 @@ const Overview = props => {
           <Typography variant="h5" className={classes.graphs_title}>
             Monthly Sales
           </Typography>
-          <MonthlySalesGraph sales={monthly_sales} />
+          {monthly_sales.loaded ?
+            <MonthlySalesGraph sales={monthly_sales.data} />
+            :
+            <CircularProgress />
+          }
         </Paper>
       </Grid>
       <Grid item md={6} xs={12}>
@@ -136,13 +145,17 @@ const Overview = props => {
           <Typography variant="h5" className={classes.graphs_title}>
             Top Clients
           </Typography>
-          <MyPieChart
-            data={top_clients.slice(0, 5)}
-            //https://coolors.co/00292d-475a5b-799496-97bec1-9de4ea
-            colors={["#00292d", "#475a5b", "#799496", "#97bec1", "#9de4ea"]}
-            pieProps={{ nameKey: "client", dataKey: "totalPurchased" }}
-            cellProps={{ stroke: "#001719" }}
-          />
+          {top_clients.loaded ?
+            <MyPieChart
+              data={top_clients.data}
+              //https://coolors.co/00292d-475a5b-799496-97bec1-9de4ea
+              colors={["#00292d", "#475a5b", "#799496", "#97bec1", "#9de4ea"]}
+              pieProps={{ nameKey: "client", dataKey: "totalPurchased" }}
+              cellProps={{ stroke: "#001719" }}
+            />
+            :
+            <CircularProgress />
+          }
         </Paper>
       </Grid>
       <Grid item md={6} xs={12}>
@@ -150,13 +163,17 @@ const Overview = props => {
           <Typography variant="h5" className={classes.graphs_title}>
             Top Regions
           </Typography>
-          <MyPieChart
-            data={top_regions.slice(0, 5)}
-            // https://coolors.co/bf211e-e82f2c-f95f5c-f99593-a06968
-            colors={["#bf211e", "#e82f2c", "#f95f5c", "#f99593", "#a06968"]}
-            pieProps={{ nameKey: "id", dataKey: "netTotal" }}
-            cellProps={{ stroke: "#7f1614" }}
-          />
+          { top_regions.loaded ?
+            <MyPieChart
+              data={top_regions.data}
+              // https://coolors.co/bf211e-e82f2c-f95f5c-f99593-a06968
+              colors={["#bf211e", "#e82f2c", "#f95f5c", "#f99593", "#a06968"]}
+              pieProps={{ nameKey: "id", dataKey: "netTotal" }}
+              cellProps={{ stroke: "#7f1614" }}
+            />
+            :
+            <CircularProgress />
+          }
         </Paper>
       </Grid>
       <Grid item xs={12}>
@@ -164,7 +181,11 @@ const Overview = props => {
           <Typography variant="h5" className={classes.graphs_title}>
             Top Products
           </Typography>
-          <TopProductsTable products={top_products.slice(0, 5)} />
+          { top_products.loaded ?
+            <TopProductsTable products={top_products.data} />
+            :
+            <CircularProgress />
+          }
         </Paper>
       </Grid>
     </Grid>
